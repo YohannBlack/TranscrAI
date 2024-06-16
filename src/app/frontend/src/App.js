11de79f5b75119908f5f9411eb22bf851c48b895
salimlounaci <salim@Salim>
2024-06-16 16:18:30 +0200

    frontend

---
+++ src/app/frontend/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [audioFile, setAudioFile] = useState(null);
+  const [transcription, setTranscription] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleFileChange = (e) => {
+    setAudioFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("audioFile", audioFile);
+
+    try {
+      const response = await fetch("http://127.0.0.1:5000/api/v1/transcribe", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setTranscription(data.transcription);
+      setError(null); // Réinitialisez l'état d'erreur en cas de succès
+    } catch (error) {
+      setError(error.message);
+      console.error("Error uploading file:", error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Upload an audio file to transcribe:</p>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleUpload}>Upload</button>
+        {error && (
+          <div className="error-message">
+            <p>Error:</p>
+            <p>{error}</p>
+          </div>
+        )}
+        {transcription && (
+          <div className="transcription-result">
+            <p>Transcription:</p>
+            <p>{transcription}</p>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
