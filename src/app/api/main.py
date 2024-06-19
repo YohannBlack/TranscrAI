d1892cef2abfc555c8facabb151945ffc68becab
YohannBlack <blackburnyohann@gmail.com>
2024-06-19 23:04:34 +0200

    /api/v1/transcribe changed to manage longer audio

--- src/app/api/main.py
+++ src/app/api/main.py
@@ -1,10 +1,10 @@
 import os
 import grpc
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response, stream_with_context
 from flask_cors import CORS
 
-from tools.helper_functions import check_audio_format
+from tools.helper_functions import check_audio_format, split_audio
 from gRPC_files.transcription.transcription_pb2 import TranscriptionRequest
 from gRPC_files.transcription.transcription_pb2_grpc import TranscribeStub
 from gRPC_files.translation.translation_pb2 import TranslationRequest
@@ -21,7 +21,9 @@ os.makedirs(download_folder, exist_ok=True)
 
 transcription_service_host = os.getenv("TRANSCRIPTION_SERVICE_HOST",
                                        "localhost")
-transcription_channel = grpc.insecure_channel("localhost:50051")
+transcription_channel = grpc.insecure_channel("localhost:50051",
+                                              options=[('grpc.max_send_message_length', 100 * 1024 * 1024),
+                                                       ('grpc.max_receive_message_length', 100 * 1024 * 1024)])
 transcription_client = TranscribeStub(transcription_channel)
 
 translatation_channel = grpc.insecure_channel("localhost:50052")
@@ -44,18 +46,46 @@ def transcribe():
         return jsonify({"error": "No file selected for uploading"}, 400)
 
     if not check_audio_format(audio_file.filename):
-        return jsonify({"error": "Invalid file format"}, 400)
+        return jsonify({"error": "Invalid file format, audio should be of format .wav, .mp3 or .flac"}, 400)
+
+    is_long_audio = request.form['isLongAudio']
+
+    if is_long_audio == "true":
+        is_long_audio = True
+    else:
+        is_long_audio = False
+
+    language = request.form['language']
 
     if audio_file:
         filename = audio_file.filename
         file_path = os.path.join(app.config['DOWNLOAD_FOLDER'], filename)
         audio_file.save(file_path)
 
-        with open(file_path, "rb") as f:
-            content = f.read()
+        audio_chunks = split_audio(file_path)
 
-        transcription_request = TranscriptionRequest(audio=content)
+    def generate_response():
+        try:
+            request_iterator = (TranscriptionRequest(
+                audio=audio, language=language, isLongAudio=is_long_audio) for audio in audio_chunks)
+            response_iterator = transcription_client.TranscribeStreaming(
+                request_iterator)
 
+            for response in response_iterator:
+                if response.text is not None:
+                    yield f"transcription: {response.text}\n\n".encode("utf-8")
+        except Exception as e:
+            return jsonify({"error": str(e)}, 500)
+
+    if len(audio_chunks) != 1:
+        return Response(stream_with_context(generate_response()), content_type="text/plain")
+    else:
+        audio = audio_chunks[0]
+        transcription_request = TranscriptionRequest(
+            audio=audio,
+            language=language,
+            isLongAudio=is_long_audio
+        )
         response = transcription_client.Transcribe(transcription_request)
 
         return jsonify({
