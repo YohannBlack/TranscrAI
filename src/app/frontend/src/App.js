417b985458e53076b70396bd0f1f57bbf00b4d6c
YohannBlack <blackburnyohann@gmail.com>
2024-06-19 23:05:04 +0200

    changed front end to handle streaming response

--- src/app/frontend/src/App.js
+++ src/app/frontend/src/App.js
@@ -1,8 +1,50 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Translation from './translate';
-import Transcription from './transcription';
-import './styles/App.css'; // Assurez-vous que ce fichier existe et contient les styles globaux
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [audioFile, setAudioFile] = useState(null);
+  const [transcription, setTranscription] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleFileChange = (e) => {
+    setAudioFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("audioFile", audioFile);
+    formData.append("language", "english");
+    formData.append("isLongAudio", "True");
+
+    try {
+      const response = await fetch("http://127.0.0.1:5000/api/v1/transcribe", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const reader = response.body.getReader();
+      let output = "";
+
+      while (true) {
+        const { done, value } = await reader.read();
+
+        if (done) {
+          break;
+        }
+
+        output += new TextDecoder("utf-8").decode(value);
+        setTranscription((prevTranscription) => prevTranscription + output);
+      }
+      setError(null); // Réinitialisez l'état d'erreur en cas de succès
+    } catch (error) {
+      setError(error.message);
+      console.error("Error uploading file:", error);
+    }
+  };
 
 const App = () => {
   return (
