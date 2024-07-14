a1eda53312d994ba0d60c57374aa11036a0d868b
YohannBlack <blackburnyohann@gmail.com>
2024-07-14 10:27:24 +0200

    conflict

--- src/app/frontend/src/App.js
+++ src/app/frontend/src/App.js
@@ -11,6 +11,11 @@ function App() {
   };
 
   const handleUpload = async () => {
+    if (!audioFile) {
+      setError('Please select an audio file first');
+      return;
+    }
+
     const formData = new FormData();
     formData.append("audioFile", audioFile);
     formData.append("language", "english");
@@ -26,35 +31,49 @@ function App() {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
-      const reader = response.body.getReader();
-      let output = "";
-
-      while (true) {
-        const { done, value } = await reader.read();
-
-        if (done) {
-          break;
-        }
-
-        const transcriptionText = new TextDecoder("utf-8")
-          .decode(value)
-          .replace("transcription:", "");
-        output += transcriptionText;
-      
-        setTranscription((prevTranscription) => prevTranscription + output);
-      }
-      setError(null); // Réinitialisez l'état d'erreur en cas de succès
+      const data = await response.json();
+      setTranscription(data[0].transcription);
+      setError(null); // Reset error state on success
     } catch (error) {
       setError(error.message);
       console.error("Error uploading file:", error);
     }
   };
 
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    setAudioFile(file);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>Upload an audio file to transcribe:</p>
-        <input type="file" onChange={handleFileChange} />
+        <p>transcrire l'audio en texte</p>
+        <div
+          className="file-input-wrapper"
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
+        >
+          <input
+            type="file"
+            id="file"
+            className="file-input"
+            onChange={handleFileChange}
+          />
+          <label htmlFor="file" className="custom-file-upload">
+            Choose File or Drag and Drop Here
+          </label>
+          {audioFile ? (
+            <p className="file-name">{audioFile.name}</p>
+          ) : (
+            <p className="file-name">No file selected</p>
+          )}
+        </div>
         <button onClick={handleUpload}>Upload</button>
         {error && (
           <div className="error-message">
