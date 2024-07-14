30f50659276fa705405faa7e331deba4481e5fa9
YohannBlack <blackburnyohann@gmail.com>
2024-07-14 11:21:28 +0200

    frontend in main

--- src/app/frontend/src/App.js
+++ src/app/frontend/src/App.js
@@ -1,95 +1,58 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Translation from "./translate";
+import Transcription from "./transcription";
+import "./styles/App.css"; // Assurez-vous que ce fichier existe et contient les styles globaux
 
-function App() {
-  const [audioFile, setAudioFile] = useState(null);
-  const [transcription, setTranscription] = useState("");
-  const [error, setError] = useState(null);
-
-  const handleFileChange = (e) => {
-    setAudioFile(e.target.files[0]);
-  };
-
-  const handleUpload = async () => {
-    if (!audioFile) {
-      setError('Please select an audio file first');
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("audioFile", audioFile);
-    formData.append("language", "english");
-    formData.append("isLongAudio", "True");
-
-    try {
-      const response = await fetch("http://127.0.0.1:5000/api/v1/transcribe", {
-        method: "POST",
-        body: formData,
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      setTranscription(data[0].transcription);
-      setError(null); // Reset error state on success
-    } catch (error) {
-      setError(error.message);
-      console.error("Error uploading file:", error);
-    }
-  };
-
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
-
-  const handleDrop = (e) => {
-    e.preventDefault();
-    const file = e.dataTransfer.files[0];
-    setAudioFile(file);
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>transcrire l'audio en texte</p>
-        <div
-          className="file-input-wrapper"
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-        >
-          <input
-            type="file"
-            id="file"
-            className="file-input"
-            onChange={handleFileChange}
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="home-page">
+                <main className="home-main">
+                  <div className="instruction-text">
+                    <h2>Que voulez-vous faire ?</h2>
+                  </div>
+                  <nav className="nav-container">
+                    <ul className="nav-list">
+                      <li className="nav-item">
+                        <Link to="/transcription">
+                          <div className="nav-block">
+                            <img
+                              src={require("./image/transcrire.png")}
+                              alt="Transcription"
+                              className="nav-image"
+                            />
+                          </div>
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/translation">
+                          <div className="nav-block">
+                            <img
+                              src={require("./image/traduire.png")}
+                              alt="Traduction"
+                              className="nav-image"
+                            />
+                          </div>
+                        </Link>
+                      </li>
+                    </ul>
+                  </nav>
+                </main>
+              </div>
+            }
           />
-          <label htmlFor="file" className="custom-file-upload">
-            Choose File or Drag and Drop Here
-          </label>
-          {audioFile ? (
-            <p className="file-name">{audioFile.name}</p>
-          ) : (
-            <p className="file-name">No file selected</p>
-          )}
-        </div>
-        <button onClick={handleUpload}>Upload</button>
-        {error && (
-          <div className="error-message">
-            <p>Error:</p>
-            <p>{error}</p>
-          </div>
-        )}
-        {transcription && (
-          <div className="transcription-result">
-            <p>Transcription:</p>
-            <p>{transcription}</p>
-          </div>
-        )}
-      </header>
-    </div>
+          <Route path="/translation" element={<Translation />} />
+          <Route path="/transcription" element={<Transcription />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/app/frontend/src/transcription.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import "./styles/transcription.css";
+
+function App() {
+  const [audioFile, setAudioFile] = useState(null);
+  const [transcription, setTranscription] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleFileChange = (e) => {
+    setAudioFile(e.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!audioFile) {
+      setError("Please select an audio file first");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("audioFile", audioFile);
+
+    try {
+      const response = await fetch("http://127.0.0.1:5000/api/v1/transcribe", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setTranscription(data[0].transcription);
+      setError(null); // Reset error state on success
+    } catch (error) {
+      setError(error.message);
+      console.error("Error uploading file:", error);
+    }
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const file = e.dataTransfer.files[0];
+    setAudioFile(file);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>transcrire l'audio en texte</p>
+        <div
+          className="file-input-wrapper"
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}>
+          <input
+            type="file"
+            id="file"
+            className="file-input"
+            onChange={handleFileChange}
+          />
+          <label htmlFor="file" className="custom-file-upload">
+            Selectionner un fichier ou deposer le fichier ici
+          </label>
+          {audioFile ? (
+            <p className="file-name">{audioFile.name}</p>
+          ) : (
+            <p className="file-name">No file selected</p>
+          )}
+        </div>
+        <button
+          onClick={handleUpload}
+          style={{ display: audioFile ? "inline-block" : "none" }}>
+          Transcrire
+        </button>
+
+        {error && (
+          <div className="error-message">
+            <p>Error:</p>
+            <p>{error}</p>
+          </div>
+        )}
+        {transcription && (
+          <div className="transcription-result">
+            <p>{transcription}</p>
+          </div>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
